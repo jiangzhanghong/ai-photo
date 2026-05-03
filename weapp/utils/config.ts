49c66907cf0335dfe954143a78462b1116ce5d7e
jiangzhanghong <867189363@qq.com>
2026-05-03 21:49:13 +0800

    ip

--- weapp/utils/config.ts
+++ weapp/utils/config.ts
@@ -1,7 +1,7 @@
 // DevTools can use localhost when domain checks are disabled.
 // Real-device debugging and release builds must use an HTTPS domain
 // configured in the Mini Program request/download legal domain list.
-export const API_BASE_URL = "http://127.0.0.1:8000";
+export const API_BASE_URL = "http://114.132.185.231:8000";
 
 export const MAX_REFERENCE_IMAGES = 5;
 
